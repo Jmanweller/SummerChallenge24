639a0ad38da362af751966321ef45bd54b4e8556
Jmanweller <35712566+Jmanweller@users.noreply.github.com>
2024-03-30 00:00:41 -0700

    fixjs

--- csvtotable.js
+++ csvtotable.js
@@ -17,3 +17,17 @@ window.addEventListener("load", () => {
         }
     });
 });
+window.addEventListener("load", () => {
+    fetch("PlayerScoreboard.csv")
+        .then(res => res.text())
+        .then(csv => {
+            table.innerHTML = "";
+            for (let row of CSV.parse(csv)) {
+                let tr = table.insertRow();
+                for (let col of row) {
+                    let td = tr.insertCell();
+                    td.innerHTML = col;
+                }
+            }
+        });
+});
